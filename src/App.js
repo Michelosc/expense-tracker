406332a100bd5f4434ab4d0155767ee855f9ffd3
Michel Corrêa <micheloscbc@hotmail.com>
2021-07-27 14:46:37 -0300

    Building a first custom component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
       <h2>Let's Get Started</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
